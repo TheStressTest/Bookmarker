4bed43aa1c99cd632fcdaf9ddb4700f014e3496c
TheStressTest <73505616+TheStressTest@users.noreply.github.com>
2021-01-29 20:45:42 -0800

    Added del-log command

--- src/cogs/logger.py
+++ src/cogs/logger.py
@@ -1,4 +1,6 @@
 import discord
+import os
+
 from discord.ext import commands
 
 
@@ -28,6 +30,19 @@ class Logging(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.Cog.listener()
     async def on_shard_connect(self, shard):
         self.bot.logger.info(f'Shard connected: {shard}')
+
+    @commands.is_owner()
+    @commands.command(name='del-logs')
+    async def _del_logs(self, ctx):
+        res = await ctx.prompt('Are you sure you would like to delete the logs?')
+        if res:
+            file = discord.File(fp='src/bot.log')
+            await ctx.author.send('Backup just in case.', file=file)
+            with open('src/bot.log', 'w') as logs:
+                logs.write('')
+            await ctx.send('Successfully cleared logs.')
+        else:
+            await ctx.better_send('Aborting.')
 
 
 def setup(bot):

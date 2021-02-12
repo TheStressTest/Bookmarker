89bbc84e2e6a3878c5996c5b1f459b2a5cb7a84f
Funnylimericks <73505616+TheStressTest@users.noreply.github.com>
2021-02-11 17:33:44 -0800

    Added utils/pycached to make clean and tweaked setup.py

--- Makefile
+++ Makefile
@@ -14,6 +14,6 @@ requirements:
 	$(VENV) -m pip install -r requirements.txt
 
 clean:
-	rm -rf venv/ src/__pycache__ src/cogs/__pycache__ src/.env src/static-config.json
+	rm -rf venv/ src/__pycache__ src/cogs/__pycache__ src/.env src/static-config.json src/utils/__pycache__
 	-rm /etc/systemd/system/devbot.service
 	-git pull
--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,14 +1,17 @@
+if __name__ == '__main__':
+    print('Refrain from running this file directly, instead use the command: make')
+    exit(1)
 import json
 
+
+# used by makefile
 print('Starting setup')
 
-env = """
-postgresql=
+env ="""postgresql=
 default_prefix=~
 token=
 dev-mode=
-webhook-url=
-"""
+webhook-url="""
 
 with open('src/.env', 'w') as tf:
     tf.write(env)

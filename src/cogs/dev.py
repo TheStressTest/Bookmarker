a87af775ea6293f186f003521c39fa0bae06f949
Funnylimericks <73505616+TheStressTest@users.noreply.github.com>
2021-02-16 09:00:45 -0800

    Add dev call & change status when rebooting

--- src/cogs/dev.py
+++ src/cogs/dev.py
@@ -13,11 +13,21 @@ class DevTools(commands.Cog, command_attrs=dict(hidden=True)):
             return True
         else:
             raise commands.NotOwner()
-            
-    @commands.command(name='restart', aliases=['reboot'])
+
+    @commands.group(name='dev')
+    async def _dev(self, ctx):
+        pass
+
+    @_dev.command(name='restart', aliases=['reboot'])
     async def _restart(self, ctx):
         await ctx.send('See ya in a bit. :wave:')
+        await self.bot.change_presence(activity=discord.Game(name='Restarting...'))
         await ctx.bot.logout()
+
+    @_dev.command(name='call')
+    async def _call(self, ctx, function):
+        self.bot.dispatch(function)
+        await ctx.send('done')
 
     @commands.group(name='devmode')
     async def _devmode(self, ctx):
